b5d2cd59adfe31e055e6e94a037edd4377639794
Ayush Dubey <ayushdubey70@gmail.com>
2017-06-11 18:00:52 +0530

    Lets aim-fy!!!

--- aim.mk
+++ aim.mk
@@ -15,16 +15,17 @@
 $(call inherit-product, device/wingtech/wt88047/full_wt88047.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aim/config/common_full_phone.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := HM2014811,HM2014812,HM2014813,HM2014814,HM2014815,HM2014816,HM2014817,HM2014818,HM2014819,HM2014820,HM2014821,HM2014112,wt88047,wt86047
-
-PRODUCT_NAME := lineage_wt88047
+export AIM_BUILDTYPE=OFFICIAL
+PRODUCT_NAME := aim_wt88047
 BOARD_VENDOR := wingtech
 PRODUCT_DEVICE := wt88047
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_PROPERTY_OVERRIDES += /
+ ro.aim.maintainer=Ayush_Dubey
